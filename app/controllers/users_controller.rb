2790662e70fe2da95990bff6dd79a6614eabb51b
DGorgula <gil@dankner.org>
2021-09-13 22:37:53 +0300

    main page updated
    Signed-off-by: DGorgula <gil@dankner.org>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
-  # GET /users or /users.json
+  # GET /
+  def main
+  end
+
+# GET /users or /users.json
   def index
     @users = User.all
   end
